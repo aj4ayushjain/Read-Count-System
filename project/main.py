b601537d07482c3cd6931f678226ceb11043edf3
Ayush Jain <aj4ayushjain@gmail.com>
2020-09-16 16:26:53 +0530

    Unused library removed

--- project/main.py
+++ project/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_user, current_user, login_required, logout_user
-from . import db , socketio
-from .models import User, Book, subs
+from . import db
+from .models import User, Book
 from .books import Results
 
 main = Blueprint('main', __name__)
